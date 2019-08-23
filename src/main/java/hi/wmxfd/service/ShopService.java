04fe6fb36266207bcbc17ac5a70c4d8b12ed668c
wangqidi123 <1973928549@qq.com>
2019-08-23 19:53:24 +0800

    abc

--- src/main/java/hi/wmxfd/service/ShopService.java
+++ src/main/java/hi/wmxfd/service/ShopService.java
@@ -5,5 +5,8 @@ import hi.wmxfd.pojo.Shopping;
 import java.util.List;
 
 public interface ShopService {
+    //分页查询
     public List<Shopping> loadPage(int page,int rows);
+    //查询最大页
+    public int calcMaxPage(int rows);
 }
