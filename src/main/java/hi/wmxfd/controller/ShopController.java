dd4a3af64a09efa456119c7fd87354289dd4dd28
wangqidi123 <1973928549@qq.com>
2019-08-23 19:54:09 +0800

    abc

---
+++ src/main/java/hi/wmxfd/controller/ShopController.java
@@ -0,0 +1,36 @@
+package hi.wmxfd.controller;
+
+import hi.wmxfd.mapper.ShopMapper;
+import hi.wmxfd.pojo.Shopping;
+import hi.wmxfd.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ShopController {
+    @Autowired
+    private ShopService shopService;
+
+    @RequestMapping("loadShops")
+    public String loadShops(@RequestParam(required = false,defaultValue = "1")int page,
+                            @RequestParam(required = false,defaultValue = "5")int rows,
+                            Model model){
+        int maxPage=shopService.calcMaxPage(rows);
+        if (page<1){
+            page=maxPage;
+        }
+        if (page>maxPage){
+            page=1;
+        }
+        List<Shopping> shoppingList= shopService.loadPage(page,rows);
+        model.addAttribute("shopList",shoppingList);
+        model.addAttribute("maxPage",maxPage);
+        model.addAttribute("currentPage",page);
+        return "";
+    }
+}
