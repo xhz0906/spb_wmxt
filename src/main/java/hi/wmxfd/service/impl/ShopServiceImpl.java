3584b8d827f6426407ab1863a3f8ffb24246a456
wangqidi123 <1973928549@qq.com>
2019-08-23 21:02:12 +0800

    abc

--- src/main/java/hi/wmxfd/service/impl/ShopServiceImpl.java
+++ src/main/java/hi/wmxfd/service/impl/ShopServiceImpl.java
@@ -2,7 +2,7 @@ package hi.wmxfd.service.impl;
 
 import com.github.pagehelper.PageHelper;
 import hi.wmxfd.mapper.ShopMapper;
-import hi.wmxfd.pojo.Shopping;
+import hi.wmxfd.pojo.Shop;
 import hi.wmxfd.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ public class ShopServiceImpl implements ShopService {
     private ShopMapper shopMapper;
 
     @Override
-    public List<Shopping> loadPage(int page, int rows) {
+    public List<Shop> loadPage(int page, int rows) {
         PageHelper.startPage(page,rows);
         return shopMapper.loadPage();
     }
