f1d1c1c0655b89a464e2849e877a315a88ec8a8f
wangqidi123 <1973928549@qq.com>
2019-08-24 17:09:25 +0800

    abc

--- src/main/java/hi/wmxfd/pojo/SysUser.java
+++ src/main/java/hi/wmxfd/pojo/SysUser.java
@@ -9,9 +9,6 @@ public class SysUser implements Serializable {
     private long userId;//用户id
     private String loginName;//登陆名
     private String password;//密码
-    private int state;//状态
-    private Date createTime;//创建时间
-    private String realname;//真实姓名
     private String userEmali;//邮箱地址
 
 }
