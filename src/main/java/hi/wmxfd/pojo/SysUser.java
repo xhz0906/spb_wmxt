c3d841155026f73e18f702d970779db47abf6b07
wangqidi123 <1973928549@qq.com>
2019-08-26 19:27:46 +0800

    abc

--- src/main/java/hi/wmxfd/pojo/SysUser.java
+++ src/main/java/hi/wmxfd/pojo/SysUser.java
@@ -11,4 +11,55 @@ public class SysUser implements Serializable {
     private String password;//密码
     private String userEmali;//邮箱地址
 
+    public SysUser() {
+    }
+
+    public SysUser(long userId, String loginName, String password, String userEmali) {
+        this.userId = userId;
+        this.loginName = loginName;
+        this.password = password;
+        this.userEmali = userEmali;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserEmali() {
+        return userEmali;
+    }
+
+    public void setUserEmali(String userEmali) {
+        this.userEmali = userEmali;
+    }
+
+    @Override
+    public String toString() {
+        return "SysUser{" +
+                "userId=" + userId +
+                ", loginName='" + loginName + '\'' +
+                ", password='" + password + '\'' +
+                ", userEmali='" + userEmali + '\'' +
+                '}';
+    }
 }
