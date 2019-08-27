d566d5622ec6ffdf809d405059a3f7f1a48aaba3
wangqidi123 <1973928549@qq.com>
2019-08-27 19:59:30 +0800

    abc

--- src/main/java/hi/wmxfd/service/ShopService.java
+++ src/main/java/hi/wmxfd/service/ShopService.java
@@ -1,6 +1,7 @@
 package hi.wmxfd.service;
 
 import hi.wmxfd.pojo.Shop;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,4 +12,15 @@ public interface ShopService {
     public int calcMaxPage(int rows);
     //增加店铺
     public boolean saveShop(Shop shop);
+    //删除一个店铺
+    public boolean deleteShop(int shopId);
+    //批量删除
+    public int pldel(List<Integer> ids);
+    //根据shopId获取shop对象
+    public Shop loadByShopId(int shopId);
+    //修改
+    public boolean updateShop(Shop shop);
+    //模糊查询
+    public  List<Shop> mohuShop(@Param("shopName")String shopName);
+
 }
