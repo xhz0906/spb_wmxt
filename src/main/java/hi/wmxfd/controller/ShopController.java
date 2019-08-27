a5d3e89c37b4ab3e99ee1aaeea21b81699a1ad1a
wangqidi123 <1973928549@qq.com>
2019-08-27 20:00:42 +0800

    abc

--- src/main/java/hi/wmxfd/controller/ShopController.java
+++ src/main/java/hi/wmxfd/controller/ShopController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -43,6 +44,49 @@ public class ShopController {
         boolean bool=shopService.saveShop(shop);
         return bool?"redirect:loadShops":"error";
     }
+    @RequestMapping("deleteShop")
+    public String deleteShop(int shopId){
+        boolean bool=shopService.deleteShop(shopId);
+        return  bool?"redirect:loadShops":"error";
+    }
+    @RequestMapping("pldel")
+    public String pldel(String[] ids){
+        List<Integer> list=new ArrayList<>();
+        for (int i=0;i<ids.length;i++){
+            list.add(Integer.parseInt(ids[i]));
+        }
+        int count=shopService.pldel(list);
+        return count>0?"redirect:loadShops":"error";
+    }
+    @RequestMapping("loadByShopId")
+    public String loadByShopId(int shopId,Model model){
+        Shop shop=shopService.loadByShopId(shopId);
+        model.addAttribute("updateShop",shop);
+        return "update";
+    }
+    @RequestMapping("updateShop")
+    public String updateShop(Shop shop){
+        boolean bool=shopService.updateShop(shop);
+        return bool?"redirect:loadShops":"error";
+    }
+    @RequestMapping("muhuShop")
+    public String mohuShop(@RequestParam(required =false,defaultValue = "1")int page,
+                           @RequestParam(required = false,defaultValue = "5")int rows,
+                           Model model,String shopName){
+        int maxPage=shopService.calcMaxPage(rows);
+        if (page<1){
+            page=maxPage;
+        }
+        if (page>maxPage){
+            page=1;
+        }
+        List<Shop> shopList=shopService.mohuShop(shopName);
+        model.addAttribute("maxPage",maxPage);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("shopList",shopList);
+        return "shopmanage";
+    }
+
 
 
 }
